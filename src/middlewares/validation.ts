9126994e7708b5f8ef0ef7dc9b6d11f7e7244af5
Christian Barrios <christiandbf@hotmail.com>
2020-06-02 21:35:19 -0500

    Add express validator middleware.

---
+++ src/middlewares/validation.ts
@@ -0,0 +1,13 @@
+import { Response, Request, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+
+export default (req: Request, res: Response, next: NextFunction): void => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res
+      .status(400)
+      .json({ message: 'Not valid params', errors: errors.array() });
+    return;
+  }
+  next();
+};
