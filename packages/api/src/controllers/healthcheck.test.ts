a8b59a60f49e67b32f94caae297566c5dd3cacc4
Christian Barrios <christiandbf@hotmail.com>
2021-05-09 11:20:39 -0500

    test(healthcheck): correct test message

--- packages/api/src/controllers/healthcheck.test.ts
+++ packages/api/src/controllers/healthcheck.test.ts
@@ -1,9 +1,8 @@
 import request from 'supertest';
-
 import app from '../app';
 
 describe('GET / - a simple api healthcheck endpoint', () => {
-	it('API runing properly', () => {
+	it('API running properly', () => {
 		return request(app).get('/').expect(200, {
 			message: 'Server is running!!!'
 		});
