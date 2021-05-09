d6d240cc0cf61c4f64d3941b30032b34ee52645b
Christian Barrios <christiandbf@hotmail.com>
2021-05-08 20:36:28 -0500

    test(entity): add unit test for entity class

---
+++ packages/core/src/domain/Entity.test.ts
@@ -0,0 +1,31 @@
+import Entity from './Entity';
+
+interface Props {
+	a: number;
+}
+
+class Test extends Entity<Props> {
+	public getProps(): Props {
+		return this.props;
+	}
+}
+
+describe('Entity', () => {
+	it('Should instantiate entity without ID correctly', () => {
+		const entity = new Test({ a: 1 }, undefined);
+		expect(entity.getId()).toBeTruthy();
+		expect(entity.getProps()).toEqual({ a: 1 });
+	});
+
+	it('Should instantiate entity with ID correctly', () => {
+		const entity = new Test({ a: 1 }, 'ff6960b0-b015-11eb-8b4e-bd5c14d98229');
+		expect(entity.getId()).toBe('ff6960b0-b015-11eb-8b4e-bd5c14d98229');
+		expect(entity.getProps()).toEqual({ a: 1 });
+	});
+
+	it('Should throw error if ID is not a UUID', () => {
+		expect(() => new Test({ a: 1 }, 'ff6960b0-b015-11eb-8b4e')).toThrow(
+			'ID is not a valid UUID'
+		);
+	});
+});
